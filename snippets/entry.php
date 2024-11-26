820611c087934e807077658b907401db8a7aea37
Gaël Poupard <ffoodd@users.noreply.github.com>
2024-11-26 15:58:40 +0100

    fix(accessibility): better label for back to content links (#1)

--- snippets/entry.php
+++ snippets/entry.php
@@ -2,7 +2,7 @@
     <?php echo $note ?>
     <?php if(option('sylvainjule.footnotes.back') && option('sylvainjule.footnotes.links')): ?>
         <span class="footnotereverse">
-            <a href="#fnref-<?php echo $count ?>">
+            <a href="#fnref-<?php echo $count ?>" title="Back to content <?php echo $count ?>">
                 <?php echo option('sylvainjule.footnotes.back') ?>
             </a>
         </span>
